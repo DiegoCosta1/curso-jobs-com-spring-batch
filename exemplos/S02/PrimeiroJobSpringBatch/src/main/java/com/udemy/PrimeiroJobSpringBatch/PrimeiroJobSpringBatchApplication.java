d36509233ecf6d4a442989659c088bd41f950787
DiegoCosta1 <deegancosta@gmail.com>
2022-07-21 17:14:04 -0300

    Criação do projeto e primeiro job

---
+++ exemplos/S02/PrimeiroJobSpringBatch/src/main/java/com/udemy/PrimeiroJobSpringBatch/PrimeiroJobSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.PrimeiroJobSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroJobSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroJobSpringBatchApplication.class, args);
+	}
+
+}
