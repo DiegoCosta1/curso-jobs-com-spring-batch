97ab2cb56976d5374e4cd7e70111472ca306c608
DiegoCosta1 <deegancosta@gmail.com>
2022-08-03 14:52:17 -0300

    Criando JdbcPagingReaderJob

--- sections/05-JdbcPagingReaderJob/src/main/resources/data.sql
+++ sections/05-JdbcPagingReaderJob/src/main/resources/data.sql
@@ -1,32 +1,3 @@
-CREATE USER 'user'@'localhost' IDENTIFIED BY '123456';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost';
-
-CREATE DATABASE spring_batch;
-
-show tables;
-
-select * from batch_job_instance;
-
-select * from batch_job_execution;
-
-select * from batch_job_execution_context;
-
--- Novo banco de dados app (S05)
-CREATE DATABASE app;
-
-USE app;
-
-CREATE TABLE cliente (
-    nome TEXT,
-    sobrenome TEXT,
-    idade TEXT,
-    email TEXT
-);
-
-INSERT INTO cliente VALUES ("Joao", "Silva", "30", "joao@test.com");
-INSERT INTO cliente VALUES ("Maria", "Silva", "30", "maria@test.com");
-
 INSERT INTO cliente VALUES ("Zeca 1", "Silva", "30", "zeca1@test.com");
 INSERT INTO cliente VALUES ("Zeca 2", "Silva", "30", "zeca2@test.com");
 INSERT INTO cliente VALUES ("Zeca 3", "Silva", "30", "zeca3@test.com");
