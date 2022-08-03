96fbe59cc33159edec45994261360dbcca9dac4d
DiegoCosta1 <deegancosta@gmail.com>
2022-08-03 14:32:22 -0300

    Prática: Leitura em banco de dados baseado em cursor

--- sections/db_scripts/myscripts.sql
+++ sections/db_scripts/myscripts.sql
@@ -11,3 +11,18 @@ select * from batch_job_instance;
 select * from batch_job_execution;
 
 select * from batch_job_execution_context;
+
+-- Novo banco de dados app (S05)
+CREATE DATABASE app;
+
+USE app;
+
+CREATE TABLE cliente (
+    nome TEXT,
+    sobrenome TEXT,
+    idade TEXT,
+    email TEXT
+);
+
+INSERT INTO cliente VALUES ("Joao", "Silva", "30", "joao@test.com");
+INSERT INTO cliente VALUES ("Maria", "Silva", "30", "maria@test.com");
