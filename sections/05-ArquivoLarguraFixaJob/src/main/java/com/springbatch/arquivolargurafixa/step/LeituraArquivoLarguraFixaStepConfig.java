d563273430014501f27edd940aadcd855e50d38b
DiegoCosta1 <deegancosta@gmail.com>
2022-08-04 15:18:21 -0300

    section 5: ItemReader
    - Leitores do spring batch
    - Teoria de arquivos fat
    - Leitura de arquivo de largura fixa
    - Restart na leitura
    - Leitura de arquivo com múltiplos formatos
    - Leitura de arquivo com múltiplos tipos de registros
    - Padrão delegate
    - Leitura de vários arquivos

---
+++ sections/05-ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/step/LeituraArquivoLarguraFixaStepConfig.java
@@ -0,0 +1,32 @@
+package com.springbatch.arquivolargurafixa.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.arquivolargurafixa.dominio.Cliente;
+
+@Configuration
+public class LeituraArquivoLarguraFixaStepConfig {
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Step leituraArquivoLarguraFixaStep(ItemReader<Cliente> leituraArquivoLarguraFixaReader,
+			ItemWriter<Cliente> leituraArquivoLarguraFixaWriter) {
+		return stepBuilderFactory
+				.get("leituraArquivoLarguraFixaStep")
+				.<Cliente, Cliente>chunk(1) // -> após 'n' itens (chunk) serem processados, o batch 
+											//    faz commit dos status/metadadod, podendo reiniciar sem perder
+											//    o progresso total, então quanto maior o chunk mais progresso
+											//    será salvo com um commit, porém quanto menor o chunk mais ele 
+											//    será performático (pois acessa menos vezes no banco)
+				.reader(leituraArquivoLarguraFixaReader)
+				.writer(leituraArquivoLarguraFixaWriter)
+				.build();
+	}
+}
---
+++ sections/05-ArquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/step/LeituraArquivoMultiplosFormatosStepConfig.java
@@ -0,0 +1,57 @@
+package com.springbatch.arquivomultiplosformatos.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.arquivomultiplosformatos.dominio.Cliente;
+import com.springbatch.arquivomultiplosformatos.reader.ArquivoClienteTransacaoReader;
+
+@Configuration
+public class LeituraArquivoMultiplosFormatosStepConfig {
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Bean
+	public Step leituraArquivoMultiplosFormatosStep(
+			MultiResourceItemReader<Cliente> multiplosArquivosClienteTransacaoReader,
+			ItemWriter leituraArquivoMultiplosFormatosItemWriter) {
+		return stepBuilderFactory
+				.get("leituraArquivoMultiplosFormatosStep")
+				.chunk(1)
+				.reader(multiplosArquivosClienteTransacaoReader)
+				.writer(leituraArquivoMultiplosFormatosItemWriter)
+				.build();
+	}
+	
+//	@SuppressWarnings({ "rawtypes", "unchecked" })
+//	@Bean
+//	public Step leituraArquivoMultiplosFormatosStep(
+//			FlatFileItemReader leituraArquivoMultiplosFormatosReader,
+//			ItemWriter leituraArquivoMultiplosFormatosItemWriter) {
+//		return stepBuilderFactory
+//				.get("leituraArquivoMultiplosFormatosStep")
+//				.chunk(1)
+//				.reader(new ArquivoClienteTransacaoReader(leituraArquivoMultiplosFormatosReader))
+//				.writer(leituraArquivoMultiplosFormatosItemWriter)
+//				.build();
+//	}
+	
+//	@Bean
+//	public Step leituraArquivoMultiplosFormatosStep(
+//			FlatFileItemReader leituraArquivoMultiplosFormatosReader,
+//			ItemWriter leituraArquivoMultiplosFormatosItemWriter) {
+//		return stepBuilderFactory
+//				.get("leituraArquivoMultiplosFormatosStep")
+//				.chunk(1)
+//				.reader(leituraArquivoMultiplosFormatosReader)
+//				.writer(leituraArquivoMultiplosFormatosItemWriter)
+//				.build();
+//	}
+}
